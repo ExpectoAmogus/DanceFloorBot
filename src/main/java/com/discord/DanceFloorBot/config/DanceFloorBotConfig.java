43c0a377d846bfdb9e4948a72f2383233ec45a93
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-03-25 21:28:48 +0100

    add_intents

--- src/main/java/com/discord/DanceFloorBot/config/DanceFloorBotConfig.java
+++ src/main/java/com/discord/DanceFloorBot/config/DanceFloorBotConfig.java
@@ -4,10 +4,11 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
+import discord4j.gateway.intent.Intent;
+import discord4j.gateway.intent.IntentSet;
 import discord4j.rest.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,8 +21,10 @@ public class DanceFloorBotConfig {
     public GatewayDiscordClient gatewayDiscordClient() {
         GatewayDiscordClient client = null;
         try {
-            client = DiscordClientBuilder.create(System.getenv("token")).build()
+            client = DiscordClientBuilder.create(System.getenv("token"))
+                    .build()
                     .gateway()
+                    .setEnabledIntents(IntentSet.of(Intent.GUILD_INTEGRATIONS, Intent.GUILD_MESSAGES,Intent.GUILD_VOICE_STATES, Intent.GUILDS, Intent.GUILD_EMOJIS_AND_STICKERS,Intent.GUILD_MESSAGE_REACTIONS, Intent.GUILD_MESSAGE_TYPING))
                     .setInitialPresence(ignore -> ClientPresence
                             .online(ClientActivity.listening(" /play")))
                     .login()
