3fc8fe4e9530f277d488b45050a1313e20fa1653
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-10-07 15:55:58 +0200

    fix lavaplayer(change to fork)

--- src/main/java/com/discord/DanceFloorBot/other/GuildAudioManager.java
+++ src/main/java/com/discord/DanceFloorBot/other/GuildAudioManager.java
@@ -3,12 +3,14 @@ package com.discord.DanceFloorBot.other;
 import com.sedmelluq.discord.lavaplayer.filter.equalizer.EqualizerFactory;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import discord4j.common.util.Snowflake;
+import lombok.Getter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static com.discord.DanceFloorBot.service.AudioManager.PLAYER_MANAGER;
 
+@Getter
 public final class GuildAudioManager {
     public static final float[] BASS_BOOST =
             {0.2f, 0.15f, 0.1f, 0.05f, 0.0f, -0.05f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f, -0.1f};
@@ -31,18 +33,6 @@ public final class GuildAudioManager {
 
     public static GuildAudioManager of(final Snowflake id) {
         return MANAGERS.computeIfAbsent(id, ignored -> new GuildAudioManager());
-    }
-
-    public AudioPlayer getPlayer() {
-        return player;
-    }
-
-    public AudioTrackScheduler getScheduler() {
-        return scheduler;
-    }
-
-    public LavaPlayerAudioProvider getProvider() {
-        return provider;
     }
 
     public void bassBoost(float percentage) {
